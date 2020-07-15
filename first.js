8f2d82f7a7db067ab79b7e020bd44a5adf24f238
aakashdeveloper <ahanda205@gmail.com>
2020-07-15 09:11:34 +0530

    a

---
+++ first.js
@@ -0,0 +1,101 @@
+es5
+es6
+Ecma Script
+
+////////////////////////
+DataType
+//////////////
+String= "hi" 'hello' '4545' "5rc" "true"
+Number=54 46563565 5.55
+Boolean= true/false
+null
+undefined
+
+
+var a = "hi"
+var b =45
+var c= true
+undefined
+typeof(a)
+"string"
+typeof(b)
+"number"
+typeof(c)
+"boolean"
+var d = "443"
+undefined
+var e = 345.45
+undefined
+typeof(d)
+"string"
+typeof(e)
+"number"
+
+
+var a = 10
+var b =5
+undefined
+a+b //add
+15
+a-b
+5
+a/b
+2
+a*b
+50
+a%b
+0
+
+4%2
+0
+4%3
+1
+4%4
+0
+
+2%4
+2
+3%4
+3
+
+var a = "Hi "
+var b ="JavaScript"
+a+b   //concat
+"Hi JavaScript"
+a-b
+NaN   > not a number
+a%b
+NaN
+a/b
+NaN
+
+var a = "hi "
+undefined
+var b =10
+undefined
+a+b
+"hi 10"
+a-b
+NaN
+b+a
+"10hi "
+
+string+string = string
+string+number = string
+number+string = string
+number+number = number
+
+
+"10"+10+"10"
+"101010"
+10+10+"10"
+"2010"
+"10"+10+10
+"101010"
+
+"10"+10+"10"-1
+101009
+10+10+"10"-1
+2009
+"10"+10+10-1
+101009
