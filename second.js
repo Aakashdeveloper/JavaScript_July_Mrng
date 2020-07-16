8450b609b68ec8e054f5fdf6d241a3614b142681
aakashdeveloper <ahanda205@gmail.com>
2020-07-16 09:01:41 +0530

    a

---
+++ second.js
@@ -0,0 +1,26 @@
+true is equal to 1
+false is equal to 0
+
+true+true
+2
+true+false
+1
+true-true
+0
+true-5
+-4
+true*5
+5
+true/5
+0.2
+false/5
+0
+5/false
+Infinity
+true+"A"
+"trueA"
+
+"true"-"true"
+NaN
+"true"+"true"
+"truetrue"
---
+++ third.js
@@ -0,0 +1,60 @@
+console.log()  > debugging
+///////////////////////////////
+alert()> prompt
+alert('Form Submitted Successfully')
+undefined
+alert('things ok')
+
+/////////////////
+confirm()
+confirm("Do you wan to leave")
+true
+confirm("Do you wan to leave")
+false
+var a = confirm("Do you wan to leave")
+undefined
+a
+true
+var a = confirm("Do you wan to leave")
+undefined
+a
+false
+
+////////////
+prompt()
+prompt('What is your age')
+"24"
+prompt('What is your name')
+"jhjsdk"
+var name = prompt('What is your name')
+undefined
+name
+"Aakash"
+
+//////
+var a = prompt("enter first number")
+var b = prompt("enter second number")
+alert(parseFloat(a)+parseFloat(b))
+
+
+var a = "1"
+undefined
+var b = "2"
+undefined
+parseInt(a)
+1
+parseInt(b)
+2
+parseInt(a)+parseInt(b)
+3
+
+var a = "10.2"
+undefined
+parseInt(a)
+10
+parseFloat(a)
+10.2
+var a = "20"
+undefined
+parseFloat(a)
+20
