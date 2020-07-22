540182d4ec3662cbc32ef21f91b479daed097b96
aakashdeveloper <ahanda205@gmail.com>
2020-07-22 07:04:24 +0530

    a

---
+++ six.js
@@ -0,0 +1,18 @@
+//Persistnant and w.rt to the browser
+localStorage.setItem('0_ui',"jdhgjb7hjbehj8hu")
+undefined
+localStorage.getItem('0_ui')
+"jdhgjb7hjbehj8hu"
+localStorage.removeItem('0_ui')
+undefined
+localStorage.getItem('0_ui')
+null
+
+
+//Not Persistant wrt to the tab
+sessionStorage.setItem('03_PII','rrh5y54776th6r')
+undefined
+sessionStorage.getItem('03_PII')
+"rrh5y54776th6r"
+sessionStorage.removeItem('03_PII')
+undefined
